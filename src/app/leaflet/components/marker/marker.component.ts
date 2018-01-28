0b09fd24f511212f912ace7c8d8d3630496cb850
Guillaume Dugas <dugas.guillaume@gmail.com>
2018-01-28 18:24:02 +0100

    Marker src fix for angular

--- src/app/leaflet/components/marker/marker.component.ts
+++ src/app/leaflet/components/marker/marker.component.ts
@@ -13,8 +13,22 @@ export class MarkerComponent extends AbstractLayer implements OnInit {
   @Input()
   latlng: L.LatLng;
 
+  private get fixPattern() {
+    return '")marker-icon.png';
+  }
+
   ngOnInit() {
     this.layer = L.marker(this.latlng);
+    this.ngFix();
+  }
+
+  ngFix() {
+    this.layer.on('add', () => {
+      const htmlIcon = this.layer._icon;
+      if (htmlIcon.src.endsWith(this.fixPattern)) {
+        htmlIcon.src = htmlIcon.src.substr(0, htmlIcon.src.length - this.fixPattern.length);
+      }
+    });
   }
 
 }
